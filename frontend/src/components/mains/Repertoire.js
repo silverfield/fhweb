2e5da4c3e21e0c565d6d0cbbb20ed081f2d928b8
fero <ferohajnovic@gmail.com>
2020-02-23 20:20:38 +0000

    selection repe almost working

--- frontend/src/components/mains/Repertoire.js
+++ frontend/src/components/mains/Repertoire.js
@@ -69,17 +69,46 @@ function TickCross({
 function Tag({
     value,
     tagMap,
-    updateFilter
+    updateFilters
 }) {
     return <span 
         className="repe-tag" 
         data-toggle="tooltip" 
         title={tagMap[value]['full']}
         style={{'backgroundColor': tagMap[value]['color']}}
-        onClick={() => updateFilter('tag', value)}
+        onClick={() => updateFilters('tag', value)}
     >
         {tagMap[value]['show'] ? tagMap[value]['show'] : value}
     </span>
+}
+
+function passFilters(
+    filters, 
+    artist,
+    bt,
+    nbt,
+    tags
+) {
+    for (var i in filters) {
+        var f = filters[i];
+        if (f['name'] === 'artist' && f['value'] !== artist) {
+            return false;
+        }
+        if (f['name'] === 'bt' && bt !== f['value']) {
+            return false;
+        }
+        if (f['name'] === 'nbt' && nbt !== f['value']) {
+            return false;
+        }
+        if (f['name'] === 'orig' && (artist === 'Fero Hajnovic') !== f['value']) {
+            return false;
+        }
+        if (f['name'] === 'tag' && !(tags.includes(f['value']))) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 function Item({
@@ -88,45 +117,26 @@ function Item({
     bt=false,
     nbt=false,
     tags=[],
-    filter=null,
-    updateFilter=null
+    filters=null,
+    updateFilters=null,
+    selection=null,
+    updateSelection=null
 }) {
-    function passFilter() {
-        for (var i in filter) {
-            var f = filter[i];
-            if (f['name'] === 'artist' && f['value'] !== artist) {
-                return false;
-            }
-            if (f['name'] === 'bt' && bt !== f['value']) {
-                return false;
-            }
-            if (f['name'] === 'nbt' && nbt !== f['value']) {
-                return false;
-            }
-            if (f['name'] === 'orig' && (artist === 'Fero Hajnovic') !== f['value']) {
-                return false;
-            }
-            if (f['name'] === 'tag' && !(tags.includes(f['value']))) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    if (!passFilter()) {
+    if (!passFilters(filters, artist, bt, nbt, tags)) {
         return <></>
     }
 
+    var isSelected = selection.filter((s) => s['name'] === name && s['artist'] === artist).length > 0;
+
     return <>
-        <tr className="repe-item">
-            <td className="repe-artist" onClick={() => updateFilter('artist', artist)}>
+        <tr className={"repe-item" + (isSelected ? ' repe-selected': '')}>
+            <td className="repe-artist" onClick={() => updateFilters('artist', artist)}>
                 {artist}
             </td>
-            <td className="repe-name">
+            <td className="repe-name" onClick={() => updateSelection(artist, name)}>
                 {name}
             </td>
-            <td className="repe-props" onClick={() => updateFilter('orig', artist === 'Fero Hajnovic')}>
+            <td className="repe-props" onClick={() => updateFilters('orig', artist === 'Fero Hajnovic')}>
                 <div className="inline-flex">
                     <TickCross yes={artist === 'Fero Hajnovic'} name="Original" />  
                 </div>
@@ -140,7 +150,7 @@ function Item({
             <td className="repe-tags">
                 {tags.map((value) => {
                     return <div className="inline-flex">
-                        <Tag tagMap={tagMap} value={value} updateFilter={updateFilter}/>
+                        <Tag tagMap={tagMap} value={value} updateFilters={updateFilters}/>
                     </div>
                 })}
             </td>
@@ -153,47 +163,101 @@ export default function Repertoire({
 }) {
     repdata.sort((a, b) => a['artist'] - b['artist']);
 
-    const [filter, setFilter] = useState([])
+    const [filters, setFilters] = useState([])
+    const [selection, setSelection] = useState([]);
 
-    function updateFilter(name, value) {
-        var matchFilters = filter.filter((f) => (f['name'] === name) && (f['value'] === value));
-        
-        if (matchFilters.length > 0) {
-            setFilter(filter.filter((f) => (f['name'] !== name) || (f['value'] !== value)));
+    function updateSelection(artist, name) {
+        var matchSelection = selection.filter((s) => (s['name'] === name) && (s['artist'] === artist));
+
+        if (matchSelection.length > 0) {
+            setSelection(selection.filter((s) => (s['name'] !== name) || (s['artist'] !== artist)));
             return;
         }
 
-        var newFilter = filter.slice().concat([{
+        var newSelection = selection.slice().concat([{
+            'name': name,
+            'artist': artist
+        }])
+        setSelection(newSelection);
+    }
+
+    function selectAll() {
+        var repdataFiltered = repdata.filter((x) => passFilters(filters, x['artist'], x['bt'], x['nbt'], x['tags']));
+        setSelection(repdataFiltered.map((r) => {
+            return {
+                artist: r['artist'], name: r['name']
+            }
+        }));
+    }
+    function deselectAll() {
+        setSelection([]);
+    }
+
+    function exportSelection() {
+        var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(selection, null, 2));
+        var dlAnchorElem = document.getElementById('downloadAnchorElem');
+        dlAnchorElem.setAttribute("href", dataStr);
+        dlAnchorElem.setAttribute("download", "selection.txt");
+        dlAnchorElem.click();
+    }
+
+    function updateFilters(name, value) {
+        var matchFilters = filters.filter((f) => (f['name'] === name) && (f['value'] === value));
+        
+        if (matchFilters.length > 0) {
+            setFilters(filters.filter((f) => (f['name'] !== name) || (f['value'] !== value)));
+            return;
+        }
+
+        var newFilters = filters.slice().concat([{
             'name': name,
             'value': value
         }])
-        setFilter(newFilter);
+        setFilters(newFilters);
     }
 
     var allTags = [...new Set(repdata.map((item) => item['tags']).flat())];
     allTags.sort();
 
+    var noFilters = <div className="active-filter">
+        No filters
+    </div>
+
     return (
         <>
+            <div className="section-title">
+                My repertoire
+            </div>
+            <div className="repe-intro">
+                <p>
+                    Below is a (rather long) list of songs I play, have playes, or are
+                    planning to play - on open mics, gigs or when busking. 
+                </p>
+                <p>
+                    Some of them require a backing track or accompaniment, but for most songs
+                    I have a version which does not require a backing track - singing, guitar and a
+                    loop station are enough. Many of the songs can be also done in a pure acoustic
+                    version.
+                </p>
+
+                <a className="repe-button" onClick={() => selectAll()}>Select all</a>
+                <a className="repe-button" onClick={() => deselectAll()}>Deselect all</a>
+                <a className="repe-button" onClick={() => exportSelection()}>Export selection</a>
+                <a id="downloadAnchorElem"></a>
+            </div>
             <div className="repe-top row">
                 <div className="col-sm-4">
-                    Active filters: {filter.length === 0 ? 'none' : ''}
-                    {filter.map((f) => {
-                        var name = {
-                            'orig': 'Original',
-                            'tag': 'Tag',
-                            'artist': 'Artist',
-                            'bt': 'Backing track version',
-                            'nbt': 'Non-backing track version',
-                        }[f['name']]
-
+                    <hr></hr>
+                    <span className="active-filters-heading">Active filters</span>
+                    {filters.length === 0 ? noFilters : <></>}
+                    {filters.map((f) => {
                         var value = f['value'];
                         var text = value;
                         if (f['name'] === 'artist') {
                             text = `Only artist "${value}"`;
                         }
                         if (f['name'] === 'tag') {
-                            text = <>Tag <Tag tagMap={tagMap} value={value} updateFilter={updateFilter}/></>
+                            text = <>Tag <Tag tagMap={tagMap} value={value} updateFilters={updateFilters}/></>
                         }
                         if (f['name'] === 'orig') {
                             text = value ? 'Only originals' : 'Only covers';
@@ -205,19 +269,20 @@ export default function Repertoire({
                             text = value ? 'Has non-backing track version' : 'Does not have non-backing track version';
                         }
 
-                        return <div className="active-filter" onClick={() => updateFilter(f['name'], f['value'])}>
+                        return <div className="active-filter" onClick={() => updateFilters(f['name'], f['value'])}>
                             <span>{text}</span>
                             <span className="remove-filter">{cross}</span>
                         </div>
                     })}
-                    <hr></hr>
                 </div>
                 <div className="col-sm-8">
+                    <hr></hr>
+                    <span className="tags-heading">Tags explained</span>
                     {allTags.map((value) => {
                         return <div className="tag-div row">
                             <div className="col-3">
                                 <div className="tag-float-right">
-                                    <Tag tagMap={tagMap} value={value} updateFilter={updateFilter}/>
+                                    <Tag tagMap={tagMap} value={value} updateFilters={updateFilters}/>
                                 </div>
                             </div>
                             <div className="col-9">
@@ -244,8 +309,10 @@ export default function Repertoire({
                     bt={item['bt']} 
                     nbt={item['nbt']} 
                     tags={item['tags']}
-                    filter={filter}
-                    updateFilter={updateFilter}
+                    filters={filters}
+                    updateFilters={updateFilters}
+                    selection={selection}
+                    updateSelection={updateSelection}
                 ></Item>)}
                 </tbody>
             </table>
